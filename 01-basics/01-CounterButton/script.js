6d64b0334b38ef0ce48b45f021175b76dc802a61
oleg.bolshakov <oleg@bolshakov.net>
2020-10-04 12:17:31 +0300

    01-basics\01-CounterButton

--- 01-basics/01-CounterButton/script.js
+++ 01-basics/01-CounterButton/script.js
@@ -1,4 +1,25 @@
 import Vue from './vue.esm.browser.js';
 
-// const app = ...
+const CounterButton = {
+  data: () => ({
+    count: 0
+  }),
+  name: 'CounterButton',
+  template: '<button @click="handleClick">Clicked {{ count }} times.</button>',
+  methods: {
+    handleClick: function() {
+      this.count++
+    }
+  }
+};
+
+const app = new Vue({
+  name: 'App',
+  template: `<main id="#app">
+    <counter-button></counter-button>
+  </main>`,
+  components: { CounterButton }
+});
+
+app.$mount('#app');
 // Рекомендуется использовать МЕТОД в качестве обработчика события
