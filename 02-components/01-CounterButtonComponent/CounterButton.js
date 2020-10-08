f6f386e79ee517b0390afb9d7a323ce199cb1996
oleg.bolshakov <oleg@bolshakov.net>
2020-10-08 16:47:46 +0300

    02-components/01-CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,10 +1,29 @@
 export const CounterButton = {
+  name: 'CounterButton',
+
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: '<button type="button" @click="handleClick">{{ count }}</button>',
 
   // Компонент должен иметь входной параметр
-
+  props: {
+    count: {
+      type: Number,
+      default: function () {
+        return 0;
+      }
+    }
+  },
   // Компонент должен иметь модель
+  model: {
+    props: 'count',
+    event: 'increment',
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+    handleClick() {
+      let value = this.count + 1;
+      this.$emit('increment', value);
+    }
+  }
 };
