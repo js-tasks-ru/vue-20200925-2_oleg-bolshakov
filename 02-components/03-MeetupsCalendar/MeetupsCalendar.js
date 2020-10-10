b389912a9a6c8897ad1a78b1cdf18858cf4d6732
oleg.bolshakov <oleg@bolshakov.net>
2020-10-10 14:38:57 +0300

    02-components/03-MeetupsCalendar

--- 02-components/03-MeetupsCalendar/MeetupsCalendar.js
+++ 02-components/03-MeetupsCalendar/MeetupsCalendar.js
@@ -1,7 +1,3 @@
-/*
-  Полезные функции по работе с датой можно описать вне Vue компонента
- */
-
 export const MeetupsCalendar = {
   name: 'MeetupsCalendar',
 
@@ -9,34 +5,96 @@ export const MeetupsCalendar = {
     <div class="rangepicker__calendar">
       <div class="rangepicker__month-indicator">
         <div class="rangepicker__selector-controls">
-          <button class="rangepicker__selector-control-left"></button>
-          <div>Июнь 2020</div>
-          <button class="rangepicker__selector-control-right"></button>
+          <button
+            class="rangepicker__selector-control-left"
+            @click="changeMonth(false)"
+          ></button>
+          <div>{{ localDate }}</div>
+          <button
+            class="rangepicker__selector-control-right"
+            @click="changeMonth(true)"
+          ></button>
         </div>
       </div>
       <div class="rangepicker__date-grid">
-        <div class="rangepicker__cell rangepicker__cell_inactive">28</div>
-        <div class="rangepicker__cell rangepicker__cell_inactive">29</div>
-        <div class="rangepicker__cell rangepicker__cell_inactive">30</div>
-        <div class="rangepicker__cell rangepicker__cell_inactive">31</div>
-        <div class="rangepicker__cell">
-          1
-          <a class="rangepicker__event">Митап</a>
-          <a class="rangepicker__event">Митап</a>
+        <div
+          v-for="cell in cells"
+          :key="cell.id"  
+          class="rangepicker__cell"
+          :class="{'rangepicker__cell_inactive': !cell.active}"
+        >
+          {{ cell.day }}
+          <a class="rangepicker__event"
+            v-for="meetup in cell.meetups"
+            :key="meetup.id"
+          >{{ meetup.title }}</a>
         </div>
-        <div class="rangepicker__cell">2</div>
-        <div class="rangepicker__cell">3</div>
       </div>
     </div>
   </div>`,
 
-  // Пропсы
+  props: {
+    meetups: {
+      type: Array,
+      required: true,
+    }
+  },
 
-  // В качестве локального состояния требуется хранить что-то,
-  // что позволит определить текущий показывающийся месяц.
-  // Изначально должен показываться текущий месяц
+  data() {
+    return {
+      date: new Date(),
+      offset: 2,
+    }
+  },
 
-  // Вычислимые свойства помогут как с получением списка дней, так и с выводом информации
+  computed: {
+    localDate() {
+      let localDate = new Date(this.date).toLocaleString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+      });
+      return localDate.substring(0, localDate.length - 3); 
+    },
+    startFromSunday() {
+      return navigator.language === 'ru' ? false : true; // Это другие страны живут по библии, а у нас принято начинать неделю с понедельника.
+    },
+    cells() {
+      const dayOfWeek = this.getDayOfWeek(this.date, 1);
+      const firstDayOfMonth = this.startFromSunday ? dayOfWeek + 1 : dayOfWeek || 7;
+      const daysInMonth = this.getDaysInMonth(this.date);
+      const multiplier = Math.ceil((firstDayOfMonth - (this.offset - 1) + daysInMonth) / 7);
+      const loop = multiplier * 7;
+      const startDate = new Date(this.date.getFullYear(), this.date.getMonth(), this.offset - firstDayOfMonth);
+      return Array.from({length: loop}, (v, i) => {
+        let date = new Date(startDate).setDate(startDate.getDate() + i);
+        return {
+          id: i+1,
+          day: new Date(date).getDate(),
+          active: new Date(this.date).getMonth() === new Date(date).getMonth(),
+          meetups: this.getMeetupsByDate(date),
+        };
+      });
+    }
+  },
 
-  // Методы понадобятся для переключения между месяцами
+  methods: {
+    changeMonth(next) {
+      let month = this.date.getMonth();
+      if ( next ) {month++;} else {month--;};
+      this.date = new Date(this.date.setMonth(month));
+    },
+    getDaysInMonth(date) {
+      return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    },
+    getDayOfWeek (date, day) {
+      return new Date(date.getFullYear(), date.getMonth(), day).getDay();
+    },
+    getMeetupsByDate(date) {
+      let day = new Date(date).setHours(0,0,0,0);
+      return this.meetups.filter(meetup => {
+        return new Date(meetup.date).setHours(0,0,0,0) === day;
+      });
+    }
+  },
+
 };
