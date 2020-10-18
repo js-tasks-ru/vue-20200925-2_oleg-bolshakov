24f611fa90837dc7e114bd04f5863598375f87b6
oleg.bolshakov <oleg@bolshakov.net>
2020-10-18 14:43:01 +0300

    04-SPA/04-Layouts

--- 04-SPA/04-Layouts/components/AuthLayout.vue
+++ 04-SPA/04-Layouts/components/AuthLayout.vue
@@ -1,4 +1,11 @@
-<template></template>
+<template>
+  <div class="page page_onboarding">
+    <div class="container">
+      <h1 class="page__title text-center">Вход</h1>
+      <slot></slot>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
@@ -6,4 +13,15 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.page.page_onboarding {
+  max-width: 374px;
+  width: 100%;
+  margin: 0 auto;
+}
+@media all and (max-width: 992px) {
+  .container {
+    padding: 0 16px;
+  }
+}
+</style>
--- 04-SPA/04-Layouts/components/FormLayout.vue
+++ 04-SPA/04-Layouts/components/FormLayout.vue
@@ -1,4 +1,11 @@
-<template></template>
+<template>
+  <div class="page page_meetup-forms">
+    <div class="container">
+      <h2 class="page__title">Создать митап</h2>
+      <slot></slot>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
@@ -6,4 +13,24 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.page.page_meetup-forms {
+  padding: 70px 0;
+}
+.page__title {
+  font-family: Roboto, sans-serif;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 37px;
+  color: var(--body-color);
+  margin: 0 0 32px;
+}
+@media all and (max-width: 992px) {
+  .container {
+    padding: 0 16px;
+  }
+  .page.page_meetup-forms {
+    padding: 48px 0;
+  }
+}
+</style>
