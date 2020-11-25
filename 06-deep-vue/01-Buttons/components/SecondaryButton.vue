3486d76d699d1d536a9567d8ef35304bcb3c4019
oleg.bolshakov <oleg@bolshakov.net>
2020-11-25 16:45:16 +0300

    06-deep-vue\01-Buttons

--- 06-deep-vue/01-Buttons/components/SecondaryButton.vue
+++ 06-deep-vue/01-Buttons/components/SecondaryButton.vue
@@ -1,10 +1,19 @@
 <template>
-  <button></button>
+  <base-button
+    class="button_secondary"
+    :block="block"
+    :tag="tag"
+    v-bind="$attrs"
+    v-on="$listeners"><slot></slot></base-button>
 </template>
 
 <script>
+import BaseButton from './BaseButton';
 export default {
   name: 'SecondaryButton',
+  inheritAttrs: false,
+  components: { BaseButton },
+  props: ['block', 'tag'],
 };
 </script>
 
