c34706c9c8329ba52b1c65f30a4874012f96be99
oleg.bolshakov <oleg@bolshakov.net>
2020-11-26 14:09:09 +0300

    07-forms/01-CounterButton_last_final

--- 07-forms/01-CounterButton_last_final/components/CounterButton.vue
+++ 07-forms/01-CounterButton_last_final/components/CounterButton.vue
@@ -1,11 +1,39 @@
 <template>
-  <button></button>
+  <button type="button" @click="handleClick">{{ lcCount }}</button>
 </template>
 
 <script>
-export default {};
+export default {
+  name: 'CounterButton',
+  data() {
+    return {
+      lcCount: null,
+    };
+  },
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    }
+  },
+  watch: {
+    count: {
+      handler: function(newValue) {
+        this.lcCount = newValue;
+      },
+      immediate: true,
+    }
+  },
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+  methods: {
+    handleClick() {
+      this.$emit('increment', ++this.lcCount);
+    }
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
