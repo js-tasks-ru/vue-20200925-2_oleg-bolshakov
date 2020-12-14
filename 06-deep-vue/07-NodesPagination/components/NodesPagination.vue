2d613792ec45ab3f69fe2f2b039fd26c116df564
oleg.bolshakov <oleg@bolshakov.net>
2020-12-14 19:03:50 +0300

    06-deep-vue/07-NodesPagination

--- 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
+++ 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
@@ -1,6 +1,26 @@
 <script>
 export default {
   name: 'NodesPagination',
+  props: {
+    page: {
+      type: Number,
+      default: 1,
+    },
+    perPage: {
+      type: Number,
+      default: 5,
+    },
+  },
+  computed: {
+    nodes() {
+      let index = (this.page * this.perPage) - this.perPage;
+      let count = this.perPage;
+      return (Array.isArray(this.$slots.default)) ? [...this.$slots.default].splice(index, count) : [];
+    }
+  },
+  render(h) {
+    return h('div', this.nodes);
+  }
 };
 </script>
 
