4eb795f905b3e5018a9ad691bea593b74e906f3e
oleg.bolshakov <oleg@bolshakov.net>
2020-12-22 11:21:33 +0300

    07-forms/04-GeneratedForm

--- 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
+++ 07-forms/04-GeneratedForm/components/MeetupAgendaItemForm.vue
@@ -1,64 +1,155 @@
 <template>
   <div class="form-section form-section_inner">
-    <button type="button" class="remove-button">
+    <button type="button" class="remove-button" @click="handleRemove">
       <img src="../assets/icons/icon-trash.svg" alt="trash" />
     </button>
 
-    <div class="form-group">
-      <dropdown-button title="Тип" :options="$options.agendaItemTypes" />
-    </div>
+    <form-group label="Тип">
+      <dropdown-button
+        title="Тип"
+        :options="$options.agendaItemTypes"
+        v-model="lcAgendaItem.type"
+      />
+    </form-group>
 
     <div class="form__row">
       <div class="form__col">
-        <div class="form-group">
-          <label class="form-label">Начало</label>
-          <app-input type="time" placeholder="00:00" />
-        </div>
+        <form-group label="Начало">
+          <app-input
+            type="time"
+            placeholder="00:00"
+            v-model="startsAt"
+          />
+        </form-group>
       </div>
       <div class="form__col">
-        <div class="form-group">
-          <label class="form-label">Окончание</label>
-          <app-input type="time" placeholder="00:00" />
-        </div>
+        <form-group label="Окончание">
+          <app-input
+            type="time"
+            placeholder="00:00"
+            v-model="endsAt"
+          />
+        </form-group>
       </div>
     </div>
 
-    <div class="form-group">
-      <label class="form-label">Заголовок</label>
-      <app-input />
-    </div>
-    <div class="form-group">
-      <label class="form-label">Описание</label>
-      <app-input multiline />
-    </div>
-    <div class="form-group">
-      <label class="form-label">Язык</label>
-      <dropdown-button
-        :options="[
-          { value: null, text: 'Не указано' },
-          { value: 'RU', text: 'RU' },
-          { value: 'EN', text: 'EN' },
-        ]"
+    <form-group
+      v-for="(item, idx) in fieldSpecification"
+      :key="idx"
+      :label="item.title"
+    >
+      <component
+        :is="item.component"
+        v-bind="item.props"
+        v-bind:[item.model.prop]="lcAgendaItem[item.field]"
+        v-on:[item.model.event]="value => lcAgendaItem[item.field] = value"
       />
-    </div>
+    </form-group>
   </div>
 </template>
 
 <script>
 import AppInput from './AppInput';
 import DropdownButton from './DropdownButton';
+import FormGroup from './FormGroup';
 import {
   getAgendaItemsFieldSpecifications,
   getAgendaItemTypes,
 } from '../meetup-service';
 
+const HOURS_A_DAY = 24;
+const MINUTES_PER_HOUR = 60;
+
 export default {
   name: 'MeetupAgendaItemForm',
 
-  components: { AppInput, DropdownButton },
+  components: { AppInput, DropdownButton, FormGroup },
 
   agendaItemTypes: getAgendaItemTypes(),
   fieldSpecifications: getAgendaItemsFieldSpecifications(),
+
+  data() {
+    return {
+      lcAgendaItem: { ...this.agendaItem },
+      duration: null,
+    };
+  },
+
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    fieldSpecification() {
+      return this.$options.fieldSpecifications[this.lcAgendaItem.type];
+    },
+    startsAt: {
+      get() {
+        return this.lcAgendaItem.startsAt;
+      },
+      set(value) {
+        this.lcAgendaItem.startsAt = value;
+        this.updateEndsTime();
+      }
+    },
+    endsAt: {
+      get() {
+        return this.lcAgendaItem.endsAt;
+      },
+      set(value) {
+        this.lcAgendaItem.endsAt = value;
+        this.calculateDuration();
+      }
+    }
+  },
+
+  watch: {
+    lcAgendaItem: {
+      handler: function() {
+        this.$emit('update:agendaItem', { ...this.lcAgendaItem });
+      },
+      deep: true
+    }
+  },
+
+  methods: {
+    handleRemove() {
+      this.$emit('remove');
+    },
+    updateEndsTime() {
+      let hours = parseInt(this.startsAt.split(':')[0]) + this.duration.hours;
+      let minutes = parseInt(this.startsAt.split(':')[1]) + this.duration.minutes;
+      if (minutes >= MINUTES_PER_HOUR) {
+        minutes = minutes % MINUTES_PER_HOUR;
+        hours++;
+      };
+      hours = hours % HOURS_A_DAY;
+      hours = hours.toString().padStart(2, '0');
+      minutes = minutes.toString().padStart(2, '0');
+      this.endsAt = hours + ':' + minutes;
+    },
+    calculateDuration() {
+      let hours = parseInt(this.endsAt.split(':')[0]) - parseInt(this.startsAt.split(':')[0]),
+          minutes = parseInt(this.endsAt.split(':')[1]) - parseInt(this.startsAt.split(':')[1]);
+      if (minutes < 0) {
+        minutes = minutes + MINUTES_PER_HOUR;
+        hours--;
+      };
+      if (hours < 0) {
+        hours = hours + HOURS_A_DAY;
+      };
+      this.duration = {
+        hours,
+        minutes
+      };
+    },
+  },
+  mounted() {
+    this.calculateDuration();
+  }
 };
 </script>
 
