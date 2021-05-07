67bc4868a6ab7172920b385f682f21d6400b8f76
oleg.bolshakov <oleg@bolshakov.net>
2021-05-07 13:56:10 +0300

    08-interactions-and-logic-reuse/02-AppPagination

--- 08-interactions-and-logic-reuse/02-AppPagination/components/AppPagination.vue
+++ 08-interactions-and-logic-reuse/02-AppPagination/components/AppPagination.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <!-- Решение -->
+    <template v-for="item in cards">
+      <slot name="default" :item="item"></slot>
+    </template>
   </div>
 </template>
 
 <script>
 export default {
   name: 'AppPagination',
+  props: {
+    page: Number,
+    perPage: Number,
+    items: Array
+  },
+  computed: {
+    cards() {
+      let index = (this.page * this.perPage) - this.perPage;
+      let count = this.perPage;
+      return [...this.items].splice(index, count);
+    }
+  }
 };
 </script>
 
