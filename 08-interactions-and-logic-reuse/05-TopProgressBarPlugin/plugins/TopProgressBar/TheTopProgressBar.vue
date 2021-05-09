2d00ddf73bd8b89a526d26f5b6d3a7f4f1d89af6
oleg.bolshakov <oleg@bolshakov.net>
2021-05-09 17:18:41 +0300

    08-interactions-and-logic-reuse/05-TopProgressBarPlugin

--- 08-interactions-and-logic-reuse/05-TopProgressBarPlugin/plugins/TopProgressBar/TheTopProgressBar.vue
+++ 08-interactions-and-logic-reuse/05-TopProgressBarPlugin/plugins/TopProgressBar/TheTopProgressBar.vue
@@ -1,10 +1,63 @@
 <template>
-  <div class="progress" style="width: 0%"></div>
+  <div class="progress" :class="classObject" :style="{ width: width + '%'}"></div>
 </template>
 
 <script>
+import TWEEN from './vendor/tween.esm';
+
+const START_VALUE = 0;
+const END_VALUE = 100;
+const DURATION = 30 * 1000;
+const DELAY = 100;
+
 export default {
   name: 'TheTopProgressBar',
+  data() {
+    return {
+      width: START_VALUE,
+      pending: false,
+      classObject: { show: false, failed: false },
+      tween: new TWEEN.Tween({ x: START_VALUE }).to({ x: END_VALUE }, DURATION).delay(DELAY)
+    }
+  },
+  methods: {
+    start() {
+      if ( !this.pending ) {
+        this.width = START_VALUE;
+        this.classObject.show = true;
+        this.classObject.failed = false;
+        this.loading();
+      }
+    },
+    finish() {
+      this.tween.stop();
+      this.width = END_VALUE;
+      this.classObject.show = false;
+    },
+    fail() {
+      this.classObject.failed = true;
+      setTimeout(() => {
+        this.finish();
+      }, 0);
+    },
+    loading() {
+      var vm = this;
+      function animate () {
+        if (TWEEN.update()) {
+          requestAnimationFrame(animate)
+        }
+      }
+      this.tween.easing(TWEEN.Easing.Exponential.Out)
+        .onUpdate(function (object) {
+          vm.width = object.x
+        })
+        .onStart(() => this.pending = true)
+        .onStop(() => this.pending = false)
+        .onComplete(() => this.pending = false)
+        .start()
+      animate()
+    }
+  }
 };
 </script>
 
--- 08-interactions-and-logic-reuse/05-TopProgressBarPlugin/plugins/TopProgressBar/index.js
+++ 08-interactions-and-logic-reuse/05-TopProgressBarPlugin/plugins/TopProgressBar/index.js
@@ -1,1 +1,28 @@
-export default {};
+export default {
+  // Экземпляр компонента TopProgressBar. Может быть вне объекта.
+  instance: null, 
+
+  // Можно добавить метод сохранения экземпляра
+  setInstance(instance) {
+    this.instance = instance;
+  }, 
+
+  // Метод запуска прогресса
+  start() { 
+    // Взаимодействует с экземпляром компонента
+    // (не забывайте о проверках на наличие экземпляра)
+    if ( typeof this.instance.start === 'function' ) {
+      this.instance.start();
+    }  
+  },
+  finish() { 
+    if ( typeof this.instance.finish === 'function' ) {
+      this.instance.finish();
+    }  
+  },
+  fail() { 
+    if ( typeof this.instance.fail === 'function' ) {
+      this.instance.fail();
+    }  
+  },
+};
